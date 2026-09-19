0e46de60eb647d92b7f7e852b715edf6793bf6a3
code-amitanshu <amitanshusingh2508@gmail.com>
2026-09-19 14:41:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# emooji converter
+
+print("😃welcome to emoji coverter😃")
+print("--------------------------------")
+text = input("enter your message:")
+
+text = text.replace(":)", "😊" )
+text = text.replace(":(", "😥")
+text = text.replace(":D", "😃")
+text = text.replace(";P", "😛")
+text = text.replace("<3", "❤")
+text = text.replace(":-)","😊")
+text = text.replace(":-(","😥")
+text = text.replace("love","❤")
+text = text.replace("happy","😊")
+text = text.replace("angry","👿")
+text = text.replace("laugh","😂")
+text = text.replace("cool","😎")
+text = text.replace("fire","💥")
+
+print("/nconverted message:")
+print(text)
+
+print("/Thank uou for using emoji converter! 😊")
